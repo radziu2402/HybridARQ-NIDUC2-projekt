5f7192f317f7fd963dbe2e37ed49f6d9800dd416
radziu2402 <radzi2002@wp.pl>
2023-03-15 00:55:19 +0100

    rebuild

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,11 +1,7 @@
 import channels.BinarySymmetricChannel;
 import channels.Channel;
 import channels.GilbertElliottChannel;
-import codes.CyclicRedundancyCheck;
-import codes.ErrorCorrectionCode;
-import codes.HammingCode;
-import codes.ParityCode;
-import messages.ErrorDetector;
+import codes.*;
 import messages.MessageGenerator;
 import messages.MessagePrinter;
 
@@ -21,101 +17,95 @@ public class MainApp {
         System.out.println("2. Gilbert-Elliott Channel");
         int channelChoice = scanner.nextInt();
 
-        System.out.println("Wybierz kod:");
-        System.out.println("1. Parity Code");
-        System.out.println("2. Hamming Code");
-        System.out.println("3. CRC8 Code");
-        System.out.println("4. CRC16 Code");
-        System.out.println("5. CRC32 Code");
-        int codeChoice = scanner.nextInt();
-
         System.out.println("Podaj długość wiadomości:");
         int messageLength = scanner.nextInt();
 
-        System.out.println("Podaj liczbę iteracji HARQ:");
-        int harqIterations = scanner.nextInt();
+        // Wybór kodu detekcyjnego
+        System.out.println("Wybierz kod detekcyjny: ");
+        System.out.println("1. Bit parzystości");
+        System.out.println("2. CRC8 Code");
+        System.out.println("3. CRC16 Code");
+        System.out.println("4. CRC32 Code");
+        int errorDetectionCodeChoice = scanner.nextInt();
 
-        System.out.println("Podaj wartość parametru dla kanału:");
-        double channelParam = scanner.nextDouble();
+        ErrorDetectionCode errorDetectionCode;
+        if (errorDetectionCodeChoice == 1) {
+            errorDetectionCode = new ParityCode();
+        } else if (errorDetectionCodeChoice == 2) {
+            int[] crc8Poly = {1, 0, 0, 1, 1, 0, 0, 0, 0}; // CRC-8 polynomial: x^8 + x^2 + x + 1
+            errorDetectionCode = new CyclicRedundancyCheck(crc8Poly);
+        } else if (errorDetectionCodeChoice == 3) {
+            int[] crc16Poly = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1}; // CRC-16 polynomial: x^16 + x^15 + x^2 + 1
+            errorDetectionCode = new CyclicRedundancyCheck(crc16Poly);
+        } else if (errorDetectionCodeChoice == 4) {
+            int[] crc32Poly = {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}; // CRC-32 polynomial: 0x04C11DB7
+            errorDetectionCode = new CyclicRedundancyCheck(crc32Poly);
+        } else {
+            System.out.println("Niepoprawny wybór kodu detekcyjnego.");
+            return;
+        }
+
+        // Wybór kodu korekcyjnego
+        System.out.println("Wybierz kod korekcyjny: ");
+        System.out.println("1. Kod Hamminga");
+        int errorCorrectionCodeChoice = scanner.nextInt();
+
+        ErrorCorrectionCode errorCorrectionCode;
+        if (errorCorrectionCodeChoice == 1) {
+            errorCorrectionCode = new HammingCode(12);
+        } else {
+            System.out.println("Niepoprawny wybór kodu korekcyjnego.");
+            return;
+        }
 
         Channel channel;
         if (channelChoice == 1) {
+            System.out.println("Podaj prawdopodobieństwo przekłamania bitu w kanale transmisyjnym (w przedziale 0-1): ");
+            double channelParam = scanner.nextDouble();
             channel = new BinarySymmetricChannel(channelParam);
         } else {
+            System.out.println("Podaj wartość pierwszego parametru dla kanału:");
+            double channelParam = scanner.nextDouble();
             System.out.println("Podaj wartość drugiego parametru dla kanału:");
             double secondChannelParam = scanner.nextDouble();
             System.out.println("Podaj wartość trzeciego parametru dla kanału:");
             double thirdChannelParam = scanner.nextDouble();
             channel = new GilbertElliottChannel(channelParam, secondChannelParam, thirdChannelParam);
         }
-
-        ErrorCorrectionCode errorCorrectionCode;
-        if (codeChoice == 1) {
-            errorCorrectionCode = new ParityCode();
-        } else if (codeChoice == 2) {
-            errorCorrectionCode = new HammingCode(messageLength);
-        } else if (codeChoice == 3) {
-            int[] crc8Poly = {1, 0, 0, 1, 1, 0, 0, 0, 0}; // CRC-8 polynomial: x^8 + x^2 + x + 1
-            errorCorrectionCode = new CyclicRedundancyCheck(crc8Poly);
-
-        } else if (codeChoice == 4) {
-            int[] crc16Poly = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1}; // CRC-16 polynomial: x^16 + x^15 + x^2 + 1
-            errorCorrectionCode = new CyclicRedundancyCheck(crc16Poly);
-
-        } else {
-            int[] crc32Poly = {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}; // CRC-32 polynomial: 0x04C11DB7
-            errorCorrectionCode = new CyclicRedundancyCheck(crc32Poly);
-        }
         System.out.println("Podaj maksymalna liczbę retransmisji");
         int maxRetransmissions = scanner.nextInt();
-        for (int i = 0; i < harqIterations; i++) {
-            System.out.println("Iteracja " + (i + 1) + ":");
-
-            boolean[] originalMessage = MessageGenerator.generateRandomMessage(messageLength);
+        int retransmissionCount = 0;
+        boolean[] detectionDecodedMessage = null;
+        boolean[] originalMessage = MessageGenerator.generateRandomMessage(messageLength);
+        while(detectionDecodedMessage==null && retransmissionCount<=maxRetransmissions) {
+            if(retransmissionCount!=0){
+                System.out.println("To już " + retransmissionCount + " retransmisja");
+            }
             System.out.println("Wiadomość oryginalna:");
             MessagePrinter.printMessage(originalMessage);
 
-            boolean[] encodedMessage = errorCorrectionCode.encode(originalMessage);
-            System.out.println("Wiadomość zakodowana:");
-            MessagePrinter.printMessage(encodedMessage);
+            boolean[] detectionEncodedMessage = errorDetectionCode.encode(originalMessage);
+            System.out.println("Wiadomość zakodowana kodem detekcyjnym:");
+            MessagePrinter.printMessage(detectionEncodedMessage);
 
-            int retransmissionCount = 0;
-            boolean[] receivedMessage = channel.transmit(encodedMessage);
+            boolean[] correctionEncodedMessage = errorCorrectionCode.encode(detectionEncodedMessage);
+            System.out.println("Wiadomość zakodowana kodem korekcyjnym:");
+            MessagePrinter.printMessage(correctionEncodedMessage);
+
+
+            boolean[] receivedMessage = channel.transmit(correctionEncodedMessage);
             System.out.println("Wiadomość odebrana:");
             MessagePrinter.printMessage(receivedMessage);
 
-            boolean[] decodedMessage = errorCorrectionCode.decode(receivedMessage);
-            System.out.println("Wiadomość zdekodowana:");
-            MessagePrinter.printMessage(decodedMessage);
-
-            boolean hasError = ErrorDetector.detectErrors(originalMessage, decodedMessage);
-            while (hasError) {
-                retransmissionCount++;
-                if (retransmissionCount > maxRetransmissions) {
-                    System.out.println("Przekroczono maksymalną liczbę retransmisji.");
-                    break;
-                }
-                System.out.println("Wystąpił błąd w transmisji.");
-                System.out.println("Wysłanie pakietu ponownie (retransmisja " + retransmissionCount + ")");
-                encodedMessage = errorCorrectionCode.encode(originalMessage);
-                receivedMessage = channel.transmit(encodedMessage);
-                System.out.println("Wiadomość odebrana:");
-                MessagePrinter.printMessage(receivedMessage);
-                decodedMessage = errorCorrectionCode.decode(receivedMessage);
-                System.out.println("Wiadomość zdekodowana:");
-                MessagePrinter.printMessage(decodedMessage);
-
-                hasError = ErrorDetector.detectErrors(originalMessage, decodedMessage);
-                if (hasError) {
-                    System.out.println("Wystąpił błąd w transmisji po ponownym wysłaniu pakietu.");
-                } else {
-                    System.out.println("Transmisja przebiegła pomyślnie po ponownym wysłaniu pakietu.");
-                }
+            boolean[] correctionDecodedMessage = errorCorrectionCode.decode(receivedMessage);
+            detectionDecodedMessage = errorDetectionCode.decode(correctionDecodedMessage);
+            if (detectionDecodedMessage != null) {
+                System.out.println("Wiadomość zdekodowana");
+                MessagePrinter.printMessage(detectionDecodedMessage);
             }
-            if (!hasError) {
-                if(retransmissionCount != 0)
-                System.out.println("Transmisja przebiegła pomyślnie po " + retransmissionCount + " retransmisjach.");
-                else System.out.println("Transmisja przebiegła pomyślnie bez retransmisji.");
+            else{
+                System.out.println("Potrzebna retransmisja");
+                retransmissionCount++;
             }
         }
     }
